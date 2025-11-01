414c9031f6bf8872ca44dc3c3a27d6b63c3089d9
Sebastian <sbernal@unsa.edu.pe>
2025-10-31 21:51:50 -0500

    Ejercicio 11

---
+++ ejercicio11.js
@@ -0,0 +1,9 @@
+let auto={
+    marca: "Toyota",
+    modelo:"Modelo",
+    año: 2025
+};
+function detalles(){
+    console.log("Marca: "+auto.marca+", Modelo: "+auto.modelo+", Año: "+auto.año);
+}
+detalles();
